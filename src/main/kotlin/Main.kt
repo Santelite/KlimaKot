4e4108de3737959c32a37489ba22d4d91a82f704
Luis Santeliz <40473385+Santelite@users.noreply.github.com>
2025-03-15 15:13:31 -0600

    crear archivo de configuracion para preguntar ubicacion y preguntarle al usuario si esta vacío, arreglar redundancia con la libreria de http

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,31 @@
 package org.example
 
-import khttp.get //no se porque IntelliJ piensa que es innecesario si claramente se utiliza esta función.
+import khttp.get
 import khttp.responses.Response
 import org.json.JSONObject
+import java.io.File
 
 fun main() {
-    println("Ingrese Ciudad o codigo postal de US")
+    val inicial = File("config.txt")
+    //if(inicial.exists())
     val postal = readln()//le pide al usuario una ciudad o codigo postal, probablemente debería crear un manejador de errores.
-    val mensaje : Response = khttp.get(
+    //val configuracion = File("config.txt")
+    if(inicial.length() == 0L) {
+        print("\n" +
+                "\n" +
+                "   _____             __ _           _____       _      _       _ \n" +
+                "  / ____|           / _(_)         |_   _|     (_)    (_)     | |\n" +
+                " | |     ___  _ __ | |_ _  __ _      | |  _ __  _  ___ _  __ _| |\n" +
+                " | |    / _ \\| '_ \\|  _| |/ _` |     | | | '_ \\| |/ __| |/ _` | |\n" +
+                " | |___| (_) | | | | | | | (_| |_   _| |_| | | | | (__| | (_| | |\n" +
+                "  \\_____\\___/|_| |_|_| |_|\\__, (_) |_____|_| |_|_|\\___|_|\\__,_|_|\n" +
+                "                           __/ |                                 \n" +
+                "                          |___/                                  \n" +
+                "\n")
+        println("Ingrese Ciudad o codigo postal de US")
+        File("config.txt").appendText(readln())
+    }
+    val mensaje : Response = get(
         url = "http://api.weatherapi.com/v1/forecast.json",
         params = mapOf("key" to "9bcc50a7119f44be93c130505251103", "q" to postal)//que buena idea dejar la llave sin obfuscar.
     )//Pedirle el JSON a la API, el cual contiene todos los datos que queremos.
@@ -31,7 +49,6 @@ fun main() {
     println("La temperatura actual es de: "+actual["temp_c"]+"°C")
     println("La sensación termica es de: "+actual["feelslike_c"]+"°C")
     println("La información fue actualizada el: "+actual["last_updated"])
-    println(" ")
     println("Información de debug")
     println(actual)
     println(futuro)
