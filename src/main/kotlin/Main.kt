cf963acee227ad7887022d248b4ea9499d16d5c3
Luis Santeliz <40473385+Santelite@users.noreply.github.com>
2025-03-15 17:34:08 -0600

    ahora si si sirve el archivo de config jajjakkajakajkajkajakjakjakjkaj

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,7 @@ import java.io.File
 
 fun main() {
     val inicial = File("config.txt")
-    //if(inicial.exists())
-    val postal = readln()//le pide al usuario una ciudad o codigo postal, probablemente debería crear un manejador de errores.
-    //val configuracion = File("config.txt")
+    var mensaje : Response
     if(inicial.length() == 0L) {
         print("\n" +
                 "\n" +
@@ -23,17 +21,56 @@ fun main() {
                 "                          |___/                                  \n" +
                 "\n")
         println("Ingrese Ciudad o codigo postal de US")
-        File("config.txt").appendText(readln())
+        val configurar = readln()
+        File("config.txt").appendText(configurar)
+        println("Guardando...")
     }
-    val mensaje : Response = get(
-        url = "http://api.weatherapi.com/v1/forecast.json",
-        params = mapOf("key" to "9bcc50a7119f44be93c130505251103", "q" to postal)//que buena idea dejar la llave sin obfuscar.
-    )//Pedirle el JSON a la API, el cual contiene todos los datos que queremos.
-    println(mensaje)//puro debug
 
+    var postal = inicial.readLines().toString()
+    var objeto : JSONObject
+    while(true) {
+        //Hacer pedido a la api
+        println(postal)
+        mensaje = get(
+            url = "http://api.weatherapi.com/v1/forecast.json",
+            params = mapOf(
+                "key" to "9bcc50a7119f44be93c130505251103",
+                "q" to postal
+            )
+        )
+        println(mensaje)
+
+
+        objeto = mensaje.jsonObject
+        println(objeto)
+
+        //File("in.json").writeText(mensaje.jsonObject.toString())
+        if(mensaje.statusCode == 200){
+            break
+        }
+
+        print(
+            "\n" +
+                    "\n" +
+                    "  ______                     _ \n" +
+                    " |  ____|                   | |\n" +
+                    " | |__   _ __ _ __ ___  _ __| |\n" +
+                    " |  __| | '__| '__/ _ \\| '__| |\n" +
+                    " | |____| |  | | | (_) | |  |_|\n" +
+                    " |______|_|  |_|  \\___/|_|  (_)\n" +
+                    "                               \n" +
+                    "                               \n" +
+                    "\n"
+        )
+        println("Ciudad Inválida! Intentalo de nuevo.")
+        inicial.delete()
+        val configurar = readln()
+        File("config.txt").appendText(configurar)
+        postal = inicial.readLines().toString()
+        println(postal)
+    }
     /*mensaje ya tiene el objeto json pero usarlo de un solo sería una terrible idea para mi sanidad mental y para
     la barbaridad que hay que hacer despues.*/
-    val objeto : JSONObject = mensaje.jsonObject
 
     /*Como la API no lo da en un formato bonito toca hacer esta barbaridad, honestamente no quise usar for porque no supe como.
     Basicamente como la API da objetos de JSON Anidados (uno dentro de otro), lo que
@@ -41,15 +78,17 @@ fun main() {
     Esto es 0 eficiente, pero funciona hasta que quieres usar el objeto de forecast porque son como 5 objetos anidados en uno,
     pero mientras no queramos usar las predicciones no importa, es problema para Luis del futuro.*/
 
+    print("")
+
     val locacion : JSONObject = objeto.getJSONObject("location")
     val actual : JSONObject = objeto.getJSONObject("current")
-    val futuro : JSONObject = objeto.getJSONObject("forecast")
+    //val futuro : JSONObject = objeto.getJSONObject("forecast")
     println(locacion["country"])
     println(locacion["name"])
     println("La temperatura actual es de: "+actual["temp_c"]+"°C")
     println("La sensación termica es de: "+actual["feelslike_c"]+"°C")
     println("La información fue actualizada el: "+actual["last_updated"])
-    println("Información de debug")
-    println(actual)
-    println(futuro)
+    //println("Información de debug")
+    //println(actual)
+    //println(futuro)
 }
